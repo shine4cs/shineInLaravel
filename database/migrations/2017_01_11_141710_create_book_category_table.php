b6790bfeff8db1f2c3c1c8f5558f7c85618aa41d
shineshine <shine@shineshinedeMacBook-Pro.local>
2017-01-11 17:15:53 +0800

    database

--- database/migrations/2017_01_11_141710_create_book_category_table.php
+++ database/migrations/2017_01_11_141710_create_book_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBookCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('book_category', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('parent_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('order');
+            $table->string('cat_img');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('book_category');
     }
 }
---
+++ database/migrations/2017_01_11_151632_create_book_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('cat_id');
+            $table->string('thumb_image');
+            $table->decimal('price',10,2);
+            $table->string('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
